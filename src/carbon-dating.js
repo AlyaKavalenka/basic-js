32cd08361a13d0d0868c13e060670ca2ab215660
Alya <kovalenkoalinam@gmail.com>
2022-10-15 20:47:40 +0300

    init: carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,9 +17,19 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || sampleActivity < 0 || sampleActivity > 15) return false;
+  console.log("sample " + sampleActivity);
+  let numerator = Math.log(MODERN_ACTIVITY/sampleActivity);
+  let denominator = 0.693 / HALF_LIFE_PERIOD;
+  t = Math.ceil(numerator/denominator);
+  if (Number.isFinite(t)) {
+    console.log("result finite " + t);
+    return t;
+  }else{
+    console.log ("return false");
+    return false;
+  }
 }
 
 module.exports = {
