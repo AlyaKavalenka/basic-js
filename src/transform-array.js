c57b458c0c6f6a843ebd10fa6b0fb66ceba9e95e
Alya <kovalenkoalinam@gmail.com>
2022-10-16 21:48:01 +0300

    init: transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+// const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Create transformed array based on the control sequences that original
@@ -13,10 +13,73 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+ function transform(arr) {
+  console.log("init arr: " + arr);
+
+  if (Array.isArray(arr) === false) {
+    throw new Error ("'arr' parameter must be an instance of the Array!");
+  }else {
+    copyArray = [];
+    let needToSkip = false;
+    let lastElemSkiped = false;
+    let needToDuplicate = false;
+    let lastElemDuplicated = false;
+    for (let i = 0; i < arr.length; i++){
+      
+      console.log("iter: " + i + " array: " + copyArray) 
+
+      if (typeof arr[i] !== 'string') {
+        if (!needToSkip) {
+          if (needToDuplicate) {
+            copyArray.push(arr[i]);
+            needToDuplicate = false;
+            lastElemDuplicated = true;
+          }
+         copyArray.push(arr[i]);
+        } else {
+          needToSkip = false;
+          lastElemSkiped = true;
+        }
+      } else if (arr[i] === '--discard-next') {
+        needToSkip = true;        
+      } else if (arr[i] === '--discard-prev') {
+        if (!lastElemSkiped) {
+          if (copyArray.length !== 0) {
+            copyArray.splice(-1, 1);
+          }
+        } else {
+          lastElemSkiped = false;
+        }
+      } else if (arr[i] === '--double-prev') {
+        if (!lastElemSkiped){
+          if (copyArray.length !== 0) {
+            copyArray.splice(i - 1, 0 , copyArray[i - 1])
+          }
+        } else {
+          lastElemSkiped = false;
+        }
+      } else if (arr[i] === '--double-next') {
+          if (copyArray.length - 1 !== i) {
+            needToDuplicate = true;
+          }
+      } else {
+        if (!needToSkip) {
+          copyArray.push(arr[i]);
+         } else {
+           needToSkip = false;
+           lastElemSkiped = true;
+         }
+      }
+      
+    }
+    
+ 
+    console.log("output: [1, 2, 3, 1337, 1337, 1337, 4, 5]");
+    console.log("result: " + copyArray);
+    return copyArray;
+  }
 }
+
 
 module.exports = {
   transform
